a52b581e923b6d64c69dde21e8a1753d4c30da46
Faith <felominofaith@gmail.com>
2026-04-20 21:51:07 +0800

    testing

--- image-to-text/src/main.js
+++ image-to-text/src/main.js
@@ -1,2 +1,12 @@
 import './style.css'
 import javascriptLogo from './assets/javascript.svg'
+
+const { createWorker } = require('tesseract.js');
+
+const worker = await createWorker('eng');
+
+(async () => {
+  const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+  console.log(text);
+  await worker.terminate();
+})();
