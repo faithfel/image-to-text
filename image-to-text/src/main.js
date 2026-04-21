0ded95013929f6bbf599444a36d07daf9ae3ac3e
Faith <felominofaith@gmail.com>
2026-04-21 08:20:38 +0800

    chnaged api

--- image-to-text/src/main.js
+++ image-to-text/src/main.js
@@ -1,12 +1,11 @@
 import './style.css'
 import javascriptLogo from './assets/javascript.svg'
 
-const { createWorker } = require('tesseract.js');
+c// Import statement in browser:
+import scribe from 'node_modules/scribe.js-ocr/scribe.js';
+// Import statement for Node.js:
+import scribe from 'scribe.js-ocr';
 
-const worker = await createWorker('eng');
-
-(async () => {
-  const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-  console.log(text);
-  await worker.terminate();
-})();
+// Basic usage
+scribe.extractText(['https://tesseract.projectnaptha.com/img/eng_bw.png'])
+	.then((res) => console.log(res))
